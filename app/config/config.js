28ecfc95690b299ec5405f2461aa0230ea1288d5
Doncho Minkov <DonchoMinkov@gmail.com>
2017-02-28 17:26:11 +0200

    Removed unused folders

--- app/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var connectionString = process.env.CONNECTION_STRING ||
-    "mongodb://localhost/books-db";
-exports.connectionString = connectionString;
-var port = process.env.PORT || 3001;
-exports.port = port;
-var secret = "purple unicorn";
-exports.secret = secret;
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,24 @@ const ts = require("gulp-typescript");
 
 const tsProject = require("./tsconfig.json");
 
-gulp.task("build", () => {
-    gulp.src(["./app/**/*ts", "./server.ts"])
+gulp.task("compile:ts", () => {
+    gulp.src(["./src/**/*.ts"])
         .pipe(ts(tsProject))
-        .pipe(gulp.dest("/build"));
+        .pipe(gulp.dest("./build"));
 });
+
+gulp.task("compile", ["compile:ts"]);
+
+gulp.task("copy:views", () => {
+    gulp.src("./src/**/*.pug")
+        .pipe(gulp.dest("./build"));
+});
+
+gulp.task("copy:static", () => {
+    gulp.src(["./src/**/*.js", "./src/**/*.css", "./src/**/*.html"])
+        .pipe(gulp.dest("./build"));
+});
+
+gulp.task("copy", ["copy:views", "copy:static"]);
+
+gulp.task("build", ["copy", "compile"]);
